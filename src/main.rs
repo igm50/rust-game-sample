19ff0e97cc754c36f78f93f5be6f678a5bcf72e9
jintz <keisuke.igimi@gmail.com>
2020-08-08 10:54:15 +0900

    feat: 画像を表示する

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 mod state;
 
 use ggez::*;
-use state::GameState;
+use state::MainState;
 
 pub fn main() {
-    let mut state = GameState::new(std::time::Duration::new(0, 0));
+    let resource_dir = std::path::Path::new("./resources");
 
     let c = conf::Conf::new();
     let (ref mut ctx, ref mut event_loop) = ContextBuilder::new("sounder", "jintz")
+        .add_resource_path(resource_dir)
         .conf(c)
         .build()
         .unwrap();
 
-    event::run(ctx, event_loop, &mut state).unwrap();
+    let ref mut state = MainState::new(ctx).unwrap();
+
+    event::run(ctx, event_loop, state).unwrap();
 }
--- src/state.rs
+++ src/state.rs
@@ -1,26 +1,36 @@
+use cgmath;
 use ggez::event::EventHandler;
+use ggez::graphics;
 use ggez::input::mouse::MouseButton;
 use ggez::*;
-use std::time::Duration;
 
-pub struct GameState {
-  dt: Duration,
+pub struct MainState {
+  image1: graphics::Image,
 }
 
-impl GameState {
-  pub fn new(dt: Duration) -> Self {
-    Self { dt: dt }
+impl MainState {
+  pub fn new(ctx: &mut Context) -> GameResult<MainState> {
+    let image1 = graphics::Image::new(ctx, "/sample.png")?;
+
+    Ok(MainState { image1: image1 })
   }
 }
 
-impl EventHandler for GameState {
-  fn update(&mut self, ctx: &mut Context) -> GameResult {
-    self.dt = timer::delta(ctx);
+impl EventHandler for MainState {
+  fn update(&mut self, _ctx: &mut Context) -> GameResult {
     Ok(())
   }
 
-  fn draw(&mut self, _ctx: &mut Context) -> GameResult {
-    Ok(())
+  fn draw(&mut self, ctx: &mut Context) -> GameResult {
+    graphics::clear(ctx, graphics::Color::new(1.0, 1.0, 1.0, 0.0));
+
+    let dst = cgmath::Point2::new(300.0, 100.0);
+    let params = graphics::DrawParam::new()
+      .scale(mint::Vector2::from_slice(&[0.5, 0.5]))
+      .dest(dst);
+    graphics::draw(ctx, &self.image1, params)?;
+
+    graphics::present(ctx)
   }
 
   fn mouse_button_down_event(&mut self, _ctx: &mut Context, _button: MouseButton, x: f32, y: f32) {
