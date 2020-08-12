57e90b9bdbe73c23391919e531adee75ee92a386
jintz <keisuke.igimi@gmail.com>
2020-08-12 09:05:42 +0900

    feat: 画像の範囲外をドラッグしても画像が動かないようにする

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod image;
 mod state;
 
 use ggez::*;
--- src/state.rs
+++ src/state.rs
@@ -1,26 +1,21 @@
-use cgmath;
+use crate::image::Image;
 use ggez::event::EventHandler;
-use ggez::graphics;
 use ggez::input::mouse::MouseButton;
 use ggez::*;
 
 pub struct MainState {
-  image1: graphics::Image,
-  x: f32,
-  y: f32,
-  mouse_down: bool,
+  image: Image,
 }
 
 impl MainState {
   pub fn new(ctx: &mut Context) -> GameResult<MainState> {
-    let image1 = graphics::Image::new(ctx, "/sample.png")?;
+    let image = Image::new(graphics::Image::new(ctx, "/sample.png")?, 300.0, 100.0);
 
-    Ok(MainState {
-      image1: image1,
-      x: 300.0,
-      y: 100.0,
-      mouse_down: false,
-    })
+    Ok(MainState { image: image })
+  }
+
+  fn draw_params(&self) -> graphics::DrawParam {
+    self.image.draw_param()
   }
 }
 
@@ -32,33 +27,22 @@ impl EventHandler for MainState {
   fn draw(&mut self, ctx: &mut Context) -> GameResult {
     graphics::clear(ctx, graphics::Color::new(1.0, 1.0, 1.0, 0.0));
 
-    let dst = cgmath::Point2::new(self.x, self.y);
-    let params = graphics::DrawParam::new()
-      .scale(mint::Vector2::from_slice(&[0.5, 0.5]))
-      .dest(dst);
-    graphics::draw(ctx, &self.image1, params)?;
+    graphics::draw(ctx, &self.image, self.draw_params())?;
 
     graphics::present(ctx)
   }
 
-  fn mouse_button_down_event(
-    &mut self,
-    _ctx: &mut Context,
-    _button: MouseButton,
-    _x: f32,
-    _y: f32,
-  ) {
-    self.mouse_down = true;
+  fn mouse_button_down_event(&mut self, _ctx: &mut Context, _button: MouseButton, x: f32, y: f32) {
+    self.image.click(x, y);
   }
 
   fn mouse_button_up_event(&mut self, _ctx: &mut Context, _button: MouseButton, _x: f32, _y: f32) {
-    self.mouse_down = false;
+    self.image.un_click();
   }
 
   fn mouse_motion_event(&mut self, _ctx: &mut Context, _x: f32, _y: f32, dx: f32, dy: f32) {
-    if self.mouse_down {
-      self.x = self.x + dx;
-      self.y = self.y + dy;
+    if self.image.clicked {
+      self.image.add(dx, dy);
     }
   }
 }
